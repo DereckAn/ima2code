291ff7e2eecf3b5af28731d65f1104c0860be919
Dereck Angeles <dereckangeles15071998@gmail.com>
2023-12-05 14:36:49 -0700

    doing the styling

---
+++ app/form.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+const Form = () => {
+  return (
+    <form
+    className="flex flex-col gap-4"
+      onSubmit={(evt) => {
+        evt.preventDefault();
+        const form = evt.currentTarget as HTMLFormElement;
+        const url = form.elements.namedItem("url") as HTMLInputElement;
+        console.log(url.value);
+      }}
+    >
+      <Label htmlFor="url"> Introduce tu URL de la imagen </Label>
+      <Input name="url" id="url" type="text" placeholder="https://miimagen.com" />
+      <Button> Convertir </Button>
+    </form>
+  );
+};
+
+export default Form;
