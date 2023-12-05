3649ee53c5a7256c30f8f03d1fb0590b14e8fe04
Dereck Angeles <dereckangeles15071998@gmail.com>
2023-12-05 15:30:31 -0700

    Sending information (error0

--- app/form.tsx
+++ app/form.tsx
@@ -4,19 +4,29 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
-const Form = () => {
+const Form = ({
+  transformUrl2Code,
+}: {
+  transformUrl2Code: (url: string) => void;
+}) => {
   return (
     <form
-    className="flex flex-col gap-4"
+      className="flex flex-col gap-4"
       onSubmit={(evt) => {
         evt.preventDefault();
         const form = evt.currentTarget as HTMLFormElement;
         const url = form.elements.namedItem("url") as HTMLInputElement;
-        console.log(url.value);
+        // console.log(url.value);
+        transformUrl2Code(url.value);
       }}
     >
       <Label htmlFor="url"> Introduce tu URL de la imagen </Label>
-      <Input name="url" id="url" type="text" placeholder="https://miimagen.com" />
+      <Input
+        name="url"
+        id="url"
+        type="text"
+        placeholder="https://miimagen.com"
+      />
       <Button> Convertir </Button>
     </form>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,30 @@
+'use client';
 import Form from "./form";
 
 export default function Home() {
+  const transformUrl2Code = async (url: string) => {
+    const res = fetch("/api/generate-code", {
+      method: "POST",
+      body: JSON.stringify({ url }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (!(await res).ok || !(await res).body == null) {
+      throw new Error("Error al generar el codigo");
+    }
+    const reader = (await res).body!.getReader();
+    const decoder = new TextDecoder("utf-8");
+
+    while (true) {
+      const { done, value } = await reader.read();
+      const chunk = decoder.decode(value);
+      console.log(chunk);
+      if (done) break;
+
+      console.log(decoder.decode(value));
+    }
+  };
   return (
     <div className="grid grid-cols-[400px_1fr]">
       <aside className="flex flex-col justify-between min-h-screen p-4 bg-gray-600">
@@ -15,7 +39,7 @@ export default function Home() {
       </aside>
       <main>
         <section className="max-w-2xl mx-auto p-10">
-          <Form />
+          <Form transformUrl2Code={transformUrl2Code} />
         </section>
       </main>
     </div>
